6c63b4bd389098d135c0e22f9f74cf32926796e6
iniquitousriddler <iniquitousriddler@gmail.com>
2018-11-03 18:13:56 -0600

    send email

--- server/index.js
+++ server/index.js
@@ -1,18 +1,75 @@
 import express from 'express';
 import Log from 'log';
 import renderer from './helpers/renderer';
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const smtp = require('nodemailer-smtp-transport');
 
+const _ = require('lodash');
 const log = new Log('info');
 const PORT = process.env.PORT || 8000;
 const app = express();
 
+app.use(cors());
 app.use(express.static('public'));
+
+try {
+    const smtpTransport = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'OAuth2',
+            user: 'noah@denverdevshop.com',
+            clientId: '1060387934321-ko82pl7g8hftu09k6hstq4j4h5dn2i1u.apps.googleusercontent.com',
+            clientSecret: 'NX9tTX4GceqDcW5IFnH2XADP',
+            refreshToken: '1/H1DC63NBHRe1kMpNrqfcib-lYGWHRDJagUK9UwvtHuY',
+            accessToken: 'ya29.GltJBmw4W9SHgjvFHP3sDSBwBwFrSljeFMbdwwZTmLrLDYQ026RwlF5mgiX0K2nTxsud8F_Z44BlCjyUsUxwSc4WHeQXTW2j9YM2CAijSci3dz1nCFtes-4jIXA8'
+        }
+    });
+} catch (e) {
+    log.error(e);
+}
+
 app.get('*', (req, res) => {
   if (req.path === '/') {
     res.send(renderer(req));
   } else {
     res.redirect(301, '/');
   }
+});
+
+app.post('/sendEmail', (req, res) => {
+    // const body = _.pick(req.body, [
+    //     'address',
+    //     'body',
+    //     'name'
+    // ]);
+    try {
+
+
+        var mailOptions = {
+            from: 'noah@denverdevshop.com',
+            to: 'rjhilgefort@gmail.com',
+            subject: 'Hello',
+            generateTextFromHTML: true,
+            html: '<b>Hello world</b>'
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(response);
+            }
+        });
+
+        // log.info(`Successfully sent email.`);
+        // res.sendStatus(200);
+    } catch (error) {
+        log.error(`Failed to send email: ${body}, ${error.message}`);
+        res.sendStatus(400);
+    }
 });
 
 app.listen(PORT, () => {
