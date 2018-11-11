ddc4211ea06a9963e878112d6db90682c5f4f553
iniquitousriddler <iniquitousriddler@gmail.com>
2018-11-11 15:24:28 -0700

    remove log

--- server/index.js
+++ server/index.js
@@ -17,7 +17,6 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 let smtpTransport
-console.log(process.env)
 try {
     smtpTransport = nodemailer.createTransport({
         host: 'smtp.gmail.com',
