21bba982917f6aa878aa06c02d6c224324409e37
jamowelling <jameson@spiredigital.com>
2018-11-17 13:58:12 -0700

    success message on successful email submit. style could use polishing, copy needs to be improved

--- src/client/Components/Modal/Modal.js
+++ src/client/Components/Modal/Modal.js
@@ -5,12 +5,53 @@ import styles from './Modal.scss';
 import bitcoinAccepted from '../../assets/bitcoin_accepted.png';
 import coCompany from '../../assets/CO_Company.png';
 
+const ModalForm = ({ handleChange, sendEmail }) => (
+  <div className={styles.modalForm}>
+    <div className={styles.inlineInputWrapper}>
+      <input
+        placeholder='Your Name'
+        onChange={e => handleChange('name', e.target.value)}
+        type='text' />
+      <input
+        placeholder='Your Email'
+        onChange={e => handleChange('address', e.target.value)}
+        type='text' />
+    </div>
+    <input
+      placeholder='Your Project Focus'
+      onChange={e => handleChange('focus', e.target.value)}
+      type='text' />
+    <textarea
+      onChange={e => handleChange('message', e.target.value)}
+      placeholder='Your Message' />
+    <button onClick={sendEmail} className={styles.submitButton} type='submit'>
+      Submit
+    </button>
+  </div>
+);
+
+const SuccessText = ({ toggleForm }) => (
+  <div>
+    <h4>
+      Nice, we've got your info now!
+    </h4>
+    <h4>
+      Noah's gonna put some content here eventually I'm sure!
+    </h4>
+    {/* eslint-disable-next-line */}
+    <h4 onClick={toggleForm} className={styles.toggleFormLink}>
+      Show me the form again
+    </h4>
+  </div>
+);
+
 class Modal extends Component {
   state = {
     address: '',
     focus: '',
     name: '',
     message: '',
+    showForm: true,
   };
 
   handleChange = (name, value) => {
@@ -31,14 +72,22 @@ class Modal extends Component {
     Promise.resolve(response).then(result => {
       if (result.ok) {
         console.log('email successfully sent!');
+        this.setState({ showForm: false });
       } else {
         console.log('email failed to send');
       }
     });
   }
 
+  toggleForm = () => {
+    this.setState(prevState => ({
+      showForm: !prevState.showForm,
+    }));
+  }
+
   render() {
     const { open, toggleContactModal } = this.props;
+    const { showForm } = this.state;
     return (
       <div className={open ? [styles.root, styles.open].join(' ') : styles.root}>
         <div
@@ -52,7 +101,7 @@ class Modal extends Component {
             className={styles.dialogBox}
           >
             <div className={styles.closeModalIcon}>
-              {/* eslint-disable-next-line */}
+              {/* eslint-disable */}
               <span role='button' onClick={() => toggleContactModal(!open)}>X</span>
             </div>
             <div className={styles.modalContent}>
@@ -61,7 +110,18 @@ class Modal extends Component {
                 <h3>And then forgotten. She adheres to GDPR.</h3>
               </div>
               <div className={styles.modalBody}>
-                <div className={styles.modalForm}>
+                {
+                  showForm ?
+                    (
+                      <ModalForm
+                        handleChange={this.handleChange}
+                        sendEmail={this.sendEmail}
+                      />
+                    ) :
+                    <SuccessText toggleForm={this.toggleForm} />
+                }
+                {/* eslint-enable */}
+                {/* <div className={styles.modalForm}>
                   <div className={styles.inlineInputWrapper}>
                     <input
                       placeholder='Your Name'
@@ -82,7 +142,7 @@ class Modal extends Component {
                   <button onClick={this.sendEmail} className={styles.submitButton} type='submit'>
                     Submit
                   </button>
-                </div>
+                </div> */}
                 <div className={styles.modalContactInfo}>
                   <br />
                   <img src={coCompany} alt='coCompany' />
@@ -106,6 +166,15 @@ class Modal extends Component {
 Modal.propTypes = {
   open: PropTypes.bool.isRequired,
   toggleContactModal: PropTypes.func.isRequired,
+};
+
+ModalForm.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  sendEmail: PropTypes.func.isRequired,
+};
+
+SuccessText.propTypes = {
+  toggleForm: PropTypes.func.isRequired,
 };
 
 export default Modal;
