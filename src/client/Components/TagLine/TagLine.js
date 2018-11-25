d2ab2427f2d3574e39ae31d99b1e5457b0be61b8
jamowelling <jameson@spiredigital.com>
2018-11-24 20:34:15 -0700

    parallax and stuff

--- src/client/Components/TagLine/TagLine.js
+++ src/client/Components/TagLine/TagLine.js
@@ -3,26 +3,32 @@ import PropTypes from 'prop-types';
 
 import styles from './TagLine.scss';
 
-const TagLine = ({ scrollToRef }) => (
-  <div className={styles.root}>
-    <div className={styles.headline}>
-      Startup team for hire
+const TagLine = ({ scrollToRef, scrollY }) => {
+  console.log('scrollY: ', scrollY);
+  const style = {
+    bottom: `${(scrollY - 64) / 4}px`,
+  };
+  return (
+    <div className={styles.root}>
+      <div style={style} className={styles.headline}>
+        Startup team for hire
+      </div>
+      <div style={style} className={styles.followUp}>
+        We specialize in design and robust application development.
+      </div>
+      <div style={style} className={styles.followUp}>
+        <p>"A great experience working with highly motivated engineers."</p>
+        <p className={styles.attribution}>- Rob</p>
+      </div>
+      <div style={style} className={styles.buttonsWrapper}>
+        <button onClick={scrollToRef} type='button'>Meet the team!</button>
+      </div>
     </div>
-    <div className={styles.followUp}>
-      We specialize in design and robust application development.
-    </div>
-    <div className={styles.followUp}>
-      <p>"A great experience working with highly motivated engineers."</p>
-      <p className={styles.attribution}>- Rob</p>
-    </div>
-    <div className={styles.buttonsWrapper}>
-      <button onClick={scrollToRef} type='button'>Meet the team!</button>
-    </div>
-  </div>
-);
+  );
+};
 
 TagLine.propTypes = {
-  toggleModal: PropTypes.func.isRequired,
+  scrollToRef: PropTypes.func.isRequired,
 };
 
 export default TagLine;
