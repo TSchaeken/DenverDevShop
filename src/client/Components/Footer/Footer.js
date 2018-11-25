cbb873704d7d4f1ecd24661d99c5fe6809b56ac0
jamowelling <jameson@spiredigital.com>
2018-11-24 18:39:33 -0700

    styling tweaks:

--- src/client/Components/Footer/Footer.js
+++ src/client/Components/Footer/Footer.js
@@ -2,6 +2,9 @@ import React from 'react';
 
 import ContactForm from '../ContactForm';
 import styles from './Footer.scss';
+import bitcoinAccepted from '../../assets/bitcoin_accepted.png';
+import coCompany from '../../assets/CO_Company.png';
+
 
 /* eslint-disable */
 // const Footer = ({ toggleModal }) => (
@@ -23,17 +26,32 @@ import styles from './Footer.scss';
 //   </div>
 // );
 
+const Branding = () => (
+  <div className={styles.modalContactInfo}>
+    <br />
+    <img src={coCompany} alt='coCompany' />
+    <br />
+    <br />
+    <h5>Location</h5>
+    <p>Denver, CO USA</p>
+    <p>Phone 720.588.3355</p>
+    <br />
+    <img src={bitcoinAccepted} alt='bitcoinAccepted' />
+  </div>
+);
+
 const Footer = () => (
   <div className={styles.root}>
     <div className={styles.branding}>
       Here's some footer branding content
+      <Branding />
     </div>
     <div className={styles.contactFormWrapper}>
-      <h2>Contact Us Today</h2>
-      <p>You'll hear from us by tomorrow</p>
+      <h2>Your email will be shared with a golden retriever.</h2>
+      <p>And then forgotten. She adheres to GDPR.</p>
       <ContactForm />
     </div>
   </div>
-)
+);
 
 export default Footer;
