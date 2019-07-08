ece414a87772b4c0325efa66fbafe5bfffa29511
Noah Schutte <noahschutte@gmail.com>
2019-07-08 11:25:17 -0600

    update google adsense

--- server/helpers/renderer.js
+++ server/helpers/renderer.js
@@ -53,7 +53,7 @@ export default (req) => {
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
           (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-6277769631415734",
+            google_ad_client: "ca-pub-8031739923151509",
             enable_page_level_ads: true
           });
         </script>
